8877f15c3c0d134afb781d5317af2965c938f978
Kshitiz Gupta <44299394+kshitiz045@users.noreply.github.com>
2020-07-24 14:46:13 +0530

    Create super_keyword.java

---
+++ super keyword/super_keyword.java
@@ -0,0 +1,26 @@
+// Java program to explain the topic of final keyword
+
+class Animal
+{
+    String color="white";
+}
+
+class Dog extends Animal
+{
+    String color="black";
+    void printColor()
+    {
+        System.out.println(color);
+        System.out.println(super.color);
+    }
+}
+
+class Test_Super
+{
+    public static void main(String args[])
+    {
+         Dog obj  =new Dog();
+         obj.printColor();
+    }
+} 
+
